4c76123c01f04a65967ac04dbeec0512762f9132
Aalxey <infurnus555@gmail.com>
2025-11-25 11:43:34 +0545

    Task1

---
+++ week5/Task1/BankAccount.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Task1
+{
+    internal class BankAccount
+    {
+        private string accountNumber;
+        private double balance;
+
+        public string AccountNumber
+        {
+            get { return accountNumber; }
+        }
+
+        public double Balance
+        {
+            get { return balance; }
+            private set
+            {
+                if (value > 0)
+                    balance = value;
+                else
+                    Console.WriteLine("Balance cannot be 0 or negative.");
+            }
+        }
+
+        // Constructor
+        public BankAccount(string accountNumber, double initialBalance)
+        {
+            this.accountNumber = accountNumber;
+            Balance = initialBalance;
+        }
+        public void Deposit(double amount)
+        {
+            if (amount <= 0)
+            {
+                Console.WriteLine("Deposit amount must be positive.");
+                return;
+            }
+
+            balance += amount;
+            Console.WriteLine($"Deposited {amount}. New balance: {balance}");
+        }
+        public void Withdraw(double amount)
+        {
+            if (amount <= 0)
+            {
+                Console.WriteLine("Withdrawal failed: Amount must be positive.");
+            }
+            else if (amount > balance)
+            {
+                Console.WriteLine("Withdrawal failed: Insufficient balance.");
+            }
+            else
+            {
+                balance -= amount;
+                Console.WriteLine($"Withdrawal successful: {amount} withdrawn. New balance: {balance}");
+            }
+        }
+    }
+}
---
+++ week5/Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount ba = new BankAccount("123456", 1000);
+            Console.WriteLine($"Account Number: {ba.AccountNumber}");
+            Console.WriteLine($"Initial Balance: {ba.Balance}");
+            ba.Deposit(500);
+            ba.Withdraw(300);
+            Console.WriteLine($"Remaining Balance: {ba.Balance}");
+        }
+    }
+}
