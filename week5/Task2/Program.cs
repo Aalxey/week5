3ccb731178f4eb035577a8d48beee9f841f4b9d5
Aalxey <infurnus555@gmail.com>
2025-11-25 12:07:31 +0545

    Task2

---
+++ week5/Task2/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car
+            {
+                Brand = "Toyota",
+                speed = 120,
+                Seats = 5
+            };
+
+            Motorcycle myBike = new Motorcycle
+            {
+                Brand = "Honda",
+                speed = 100,
+                HasCarrier = true
+            };
+
+            Console.WriteLine("Car Actions:");
+            myCar.start();        
+            myCar.DisplayInfo();  
+            myCar.stop();         
+
+            Console.WriteLine("\nMotorcycle Actions:");
+            myBike.start();        
+            myBike.DisplayInfo();  
+            myBike.stop();
+        }
+    }
+}
---
+++ week5/Task2/Vehicle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    internal class Vehicle
+    {
+        public string Brand { get; set; }
+        public int speed {  get; set; }
+
+        public void start()
+        {
+            Console.WriteLine($"{Brand} is starting at {speed} km/h.");
+        }
+        public void stop() 
+        {
+            Console.WriteLine($"{Brand} has stopped.");
+        }
+        public virtual void DisplayInfo()
+        {
+            Console.WriteLine($"Brand: {Brand}, Speed: {speed} km/h");
+        }
+    }
+}
