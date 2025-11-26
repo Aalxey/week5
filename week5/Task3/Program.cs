6600e4cf256f000b8a7dd33d12dc7f5f1bf28e69
Aalxey <infurnus555@gmail.com>
2025-11-26 09:45:35 +0545

    Task3

---
+++ week5/Task3/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Printer myPrinter = new Printer();
+
+            myPrinter.Print("Hello, World!");       
+            myPrinter.Print(2025);                  
+            myPrinter.Print("Repeating message", 3);
+
+
+            //For teacher part
+            NepaliTeacher nepTeacher = new NepaliTeacher
+            {
+                Name = "Ram"
+            };
+
+            
+            EnglishTeacher engTeacher = new EnglishTeacher
+            {
+                Name = "Sarah"
+            };
+
+            Console.WriteLine("Nepali Teacher:");
+            Console.WriteLine($"Name: {nepTeacher.Name}");
+            nepTeacher.Teaching();     
+            nepTeacher.SalaryInfo();    
+
+            Console.WriteLine("\nEnglish Teacher:");
+            Console.WriteLine($"Name: {engTeacher.Name}");
+            engTeacher.Teaching();      
+            engTeacher.SalaryInfo();
+        }
+    }
+}
---
+++ week5/Task3/Teacher.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    internal class Teacher
+    {
+        public string Name { get; set; }
+
+        public virtual void Teaching()
+        {
+            Console.WriteLine("Teacher teaches in English");
+        }
+
+        public void SalaryInfo()
+        {
+            Console.WriteLine("Salary cannot be changed by any child class.");
+        }
+    }
+}
