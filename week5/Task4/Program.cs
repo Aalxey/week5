cab51e05904a70fb796da1a7ecf4cdcc007fbf2c
Aalxey <infurnus555@gmail.com>
2025-11-26 09:50:49 +0545

    Task4

---
+++ week5/Task4/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Task4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+            Console.WriteLine("Car:");
+            myCar.Display();        
+            myCar.StartEngine();    
+            myCar.StopEngine();     
+
+            Console.WriteLine();
+
+            
+            Bike myBike = new Bike();
+            Console.WriteLine("Bike:");
+            myBike.Display();      
+            myBike.StartEngine();   
+            myBike.StopEngine();
+        }
+    }
+}
